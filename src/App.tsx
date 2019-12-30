9b7ad31858019ac099ad01889965f4ecb9b7e274
amit090shukla <amit090shukla@gmail.com>
2019-12-30 23:50:16 +0530

    Added Sheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style.css";
 
 const App: React.FC = () => {
-  return <div className="App">Nil</div>;
+  const [pos, setPos] = useState<Array<{ x: number; y: number }>>([]);
+  const handleSheetClick = (e: any) => {
+    e.persist();
+    setPos(prev => [...prev, { x: e.clientX, y: e.clientY }]);
+  };
+  return (
+    <div className="container" onClick={handleSheetClick}>
+      {pos.map((pos, id) => (
+        <input
+          key={id}
+          autoFocus
+          placeholder="Why are you gay"
+          style={{ position: "absolute", left: pos.x, top: pos.y }}
+          onClick={e => e.stopPropagation()}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default App;
