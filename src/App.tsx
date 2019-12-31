b64a9d0e08dc038f3c27eea6146954a12b72e85b
amit090shukla <amit090shukla@gmail.com>
2019-12-31 10:48:11 +0530

    Made draggable

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,28 @@ import React, { useState } from "react";
 import "./style.css";
 
 const App: React.FC = () => {
-  const [pos, setPos] = useState<Array<{ x: number; y: number }>>([]);
+  const [pos, setPos] = useState<Array<{ x: number; y: number; id: number }>>(
+    []
+  );
+  const [currentlyDragging, setCurrentlyDragging] = useState<number | null>(
+    null
+  );
   const handleSheetClick = (e: any) => {
     e.persist();
-    setPos(prev => [...prev, { x: e.clientX, y: e.clientY }]);
+    setPos(prev => [
+      ...prev,
+      { x: e.clientX, y: e.clientY, id: e.clientY + e.clientX }
+    ]);
+  };
+  const handleDragEnd = (e: any) => {
+    const us = pos.map(pos => {
+      if (pos.id === currentlyDragging) {
+        return { x: e.clientX, y: e.clientY, id: currentlyDragging };
+      }
+      return pos;
+    });
+    setPos(us);
+    console.log(e);
   };
   return (
     <div className="container" onClick={handleSheetClick}>
@@ -13,9 +31,12 @@ const App: React.FC = () => {
         <input
           key={id}
           autoFocus
-          placeholder="Why are you gay"
           style={{ position: "absolute", left: pos.x, top: pos.y }}
           onClick={e => e.stopPropagation()}
+          className="input_styles"
+          draggable={true}
+          onDragStart={() => setCurrentlyDragging(pos.id)}
+          onDragEnd={handleDragEnd}
         />
       ))}
     </div>
